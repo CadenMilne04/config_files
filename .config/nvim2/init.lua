2060913f81f459caeb611132b6f45cdd0706bd16
Caden Milne <cmilne@sei.cmu.edu>
2024-07-19 08:25:09 -0400

    fix: Revamp of config files from my setup at SEI

--- .config/nvim2/init.lua
+++
@@ -1,2 +0,0 @@
-require("cadenmilne.core")
-require("cadenmilne.lazy")
--- nvim/lua/cadenmilne/plugins/treesitter.lua
+++ nvim/lua/cadenmilne/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return{
     config = function()
         require'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "java" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "java", "markdown", "markdown_inline"},
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
